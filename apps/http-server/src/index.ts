a4db496eacc75a812e8302711fbf871a528500ec
harsharora05 <hararora12@gmail.com>
2025-10-18 20:34:35 +0530

    fixed token docker

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -12,7 +12,7 @@ app.get("/", async (req: Request, res: Response) => {
             password: Math.random().toString()
         }
     })
-    res.json({ "message": "hii there" })
+    res.json({ "message": "hello there" })
 });
 
 app.get("/users", async (req: Request, res: Response) => {
