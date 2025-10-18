ea0fb4b77a497702ccb720897dac08bfd702ff45
harsharora05 <hararora12@gmail.com>
2025-10-18 22:38:25 +0530

    final test

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -12,7 +12,7 @@ app.get("/", async (req: Request, res: Response) => {
             password: Math.random().toString()
         }
     })
-    res.json({ "message": "hello" })
+    res.json({ "message": "chal gyaaaaaa cd" })
 });
 
 app.get("/users", async (req: Request, res: Response) => {
