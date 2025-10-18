645bdc04f94c8ae2144d14db80e13649331287fe
harsharora05 <hararora12@gmail.com>
2025-10-18 20:28:31 +0530

    init cd with docker

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express"
+import { prismaClient } from "@repo/db/client";
+
+const app = express();
+app.use(express.json());
+
+
+app.get("/", async (req: Request, res: Response) => {
+    await prismaClient.user.create({
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    })
+    res.json({ "message": "hii there" })
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+    const users = await prismaClient.user.findMany();
+    res.json({ "message": users })
+});
+
+
+app.listen(3001)
+
